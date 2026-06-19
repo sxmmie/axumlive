1173e29a9ee1e1d1727aa20a7562d5270da04dd0
Sxmmie <samuelumohjnr@gmail.com>
2026-06-19 19:09:35 +0100

    Added SQL commends for migrations, route and server config

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use axum::{
+    Router,
     extract::{Path, State},
     http::StatusCode,
     routing::{get, post},
 };
+use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPoolOptions, prelude::FromRow};
 use std::env;
 
+// This will be used to create a user
 #[derive(Deserialize)]
 struct UserPayload {
     name: String,
     email: String,
 }
 
+// Used to get a user or list of users
 #[derive(Serialize, FromRow)]
 struct User {
     id: i32,
@@ -20,5 +25,17 @@ struct User {
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new().connect(&db_url).await.expect("Failed to connect to DB");
+    sqlx::migrate!().run(&pool).await.expect("Migrations failed"); // this will go to the migrations fdirectory and run the SQL commands
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user).get(list_users))
+        .route("/users/{id}", get(get_user).put(update_user).delete(delete_user))
+        .with_state(pool);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    println!("Server running on port 8000");
+    axum::serve(listener, app).await.unwrap();
 }
